bb84e55122d4ba3a22a7a005b9360469d9624870
Yurei <yosribouabid@gmail.com>
2021-06-10 21:41:53 +0100

    path fix

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 import AppController from '../controllers/AppController';
 import UsersController from '../controllers/UsersController';
 import AuthController from '../controllers/AuthController';
+import FilesController from '../controllers/FilesController';
 
 const express = require('express');
 
@@ -12,9 +13,10 @@ const router = (app) => {
   path.get('/status', ((req, res) => AppController.getStatus(req, res)));
   path.get('/stats', ((req, res) => AppController.getStats(req, res)));
   path.post('/users', ((req, res) => UsersController.postNew(req, res)));
-  paths.get('/connect', ((req, res) => AuthController.getConnect(req, res)));
-  paths.get('/disconnect', ((req, res) => AuthController.getDisconnect(req, res)));
-  paths.get('/users/me', ((req, res) => UsersController.getMe(req, res)));
+  path.get('/connect', ((req, res) => AuthController.getConnect(req, res)));
+  path.get('/disconnect', ((req, res) => AuthController.getDisconnect(req, res)));
+  path.get('/users/me', ((req, res) => UsersController.getMe(req, res)));
+  path.post('/files', ((request, response) => FilesController.postUpload(request, response)));
 };
 
 export default router;
